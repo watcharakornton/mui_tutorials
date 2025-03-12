9286934896a9e6d2962279d00b792f606290d850
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-12 15:33:44 +0700

    Customized Switch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AutoComplete from "./inputs/AutoComplete"
 import BasicSelect from "./inputs/BasicSelect"
 import FloatingAction from "./inputs/FloatingAction"
 import SwitchComponent from "./inputs/SwitchComponent"
+import { CustomizedSwitches }  from "./inputs/CustomizedSwitches"
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       {/* <AutoComplete /> */}
       {/* <BasicSelect /> */}
       {/* <FloatingAction /> */}
-      <SwitchComponent />
+      {/* <SwitchComponent /> */}
+      <CustomizedSwitches />
     </>
   )
 }
