36fb8b348c6f1516b9437b1451270b256e062168
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-12 16:29:59 +0700

    BasicTransferList Edited

--- src/inputs/BasicTransferList.jsx
+++ src/inputs/BasicTransferList.jsx
@@ -7,6 +7,7 @@ import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 function not(a, b) {
     return a.filter((value) => !b.includes(value));
@@ -90,57 +91,60 @@ export const BasicTransferList = () => {
     );
 
     return (
-        <Grid
-            container
-            spacing={2}
-            sx={{ justifyContent: 'center', alignItems: 'center' }}
-        >
-            <Grid item>{customList(left)}</Grid>
-            <Grid item>
-                <Grid container direction="column" sx={{ alignItems: 'center' }}>
-                    <Button
-                        sx={{ my: 0.5 }}
-                        variant="outlined"
-                        size="small"
-                        onClick={handleAllRight}
-                        disabled={left.length === 0}
-                        aria-label="move all right"
-                    >
-                        ≫
-                    </Button>
-                    <Button
-                        sx={{ my: 0.5 }}
-                        variant="outlined"
-                        size="small"
-                        onClick={handleCheckedRight}
-                        disabled={leftChecked.length === 0}
-                        aria-label="move selected right"
-                    >
-                        &gt;
-                    </Button>
-                    <Button
-                        sx={{ my: 0.5 }}
-                        variant="outlined"
-                        size="small"
-                        onClick={handleCheckedLeft}
-                        disabled={rightChecked.length === 0}
-                        aria-label="move selected left"
-                    >
-                        &lt;
-                    </Button>
-                    <Button
-                        sx={{ my: 0.5 }}
-                        variant="outlined"
-                        size="small"
-                        onClick={handleAllLeft}
-                        disabled={right.length === 0}
-                        aria-label="move all left"
-                    >
-                        ≪
-                    </Button>
+        <Box paddingY={3}>
+            <Grid
+                container
+                spacing={2}
+                sx={{ justifyContent: 'center', alignItems: 'center' }}
+            >
+                <Grid item>{customList(left)}</Grid>
+                <Grid item>
+                    <Grid container direction="column" sx={{ alignItems: 'center' }}>
+                        <Button
+                            sx={{ my: 0.5 }}
+                            variant="outlined"
+                            size="small"
+                            onClick={handleAllRight}
+                            disabled={left.length === 0}
+                            aria-label="move all right"
+                        >
+                            ≫
+                        </Button>
+                        <Button
+                            sx={{ my: 0.5 }}
+                            variant="outlined"
+                            size="small"
+                            onClick={handleCheckedRight}
+                            disabled={leftChecked.length === 0}
+                            aria-label="move selected right"
+                        >
+                            &gt;
+                        </Button>
+                        <Button
+                            sx={{ my: 0.5 }}
+                            variant="outlined"
+                            size="small"
+                            onClick={handleCheckedLeft}
+                            disabled={rightChecked.length === 0}
+                            aria-label="move selected left"
+                        >
+                            &lt;
+                        </Button>
+                        <Button
+                            sx={{ my: 0.5 }}
+                            variant="outlined"
+                            size="small"
+                            onClick={handleAllLeft}
+                            disabled={right.length === 0}
+                            aria-label="move all left"
+                        >
+                            ≪
+                        </Button>
+                    </Grid>
                 </Grid>
+                <Grid item>{customList(right)}</Grid>
             </Grid>
-            <Grid item>{customList(right)}</Grid>
-        </Grid>
+
+        </Box>
     );
 }
