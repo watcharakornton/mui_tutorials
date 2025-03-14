cfe827cfff59c57df463e716b4ea55358127db6f
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-15 01:51:08 +0700

    Redesign Checkbox Page

--- src/pages/CheckboxPage.jsx
+++ src/pages/CheckboxPage.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Container, Stack, Divider, Button } from '@mui/material';
+import { Container, Grid, Divider, Button, Typography } from '@mui/material';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 
 import {
@@ -8,25 +8,36 @@ import {
 } from "../inputs/CheckboxComponent";
 
 const CheckboxPage = () => {
-    const sections = [<Checkboxes />, <CheckboxLabels />];
+    const sections = [
+        <Checkboxes />, 
+        <CheckboxLabels />,
+    ];
 
     return (
         <Container maxWidth="sm">
-            <Stack
-                spacing={3}
+            <Grid
+                container
                 alignItems="center"
                 justifyContent="center"
                 sx={{ minHeight: "100vh" }}
             >
-                <Divider sx={{ width: "100%" }} />
+                <Grid item xs={12}>
+                    <Typography variant='h3' align='center' sx={{ mt: 5 }}>
+                        Checkbox Page
+                    </Typography>
+                </Grid>
                 {sections.map((Component, index) => (
                     <React.Fragment key={index}>
-                        {Component}
-                        <Divider sx={{ width: "100%" }} />
+                        <Grid item xs={12} sx={{ mt: 5 }}>
+                            {Component}
+                            <Divider sx={{ mt: 3, width: "100%" }} />
+                        </Grid>
                     </React.Fragment>
                 ))}
-                <Button size='large' startIcon={<ChevronLeftIcon />} href="/">Back</Button>
-            </Stack>
+                <Grid item xs={12} sx={{ my: 5 }}>
+                    <Button size='large' startIcon={<ChevronLeftIcon />} href="/">Back</Button>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
