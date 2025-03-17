b73d5d6c6ee69ebb5dde231c16be110e09d0e8a3
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-17 15:00:36 +0700

    Add Floating Action Button Zoom into FAB Component

--- src/pages/FloatingActionButtonPage.jsx
+++ src/pages/FloatingActionButtonPage.jsx
@@ -6,6 +6,7 @@ import {
     FloatingActionButtons,
     FloatingActionButtonSize,
     FloatingActionButtonExtendedSize,
+    FloatingActionButtonZoom,
 } from "../inputs/FloatingActionButtonComponent";
 
 const CheckboxPage = () => {
@@ -13,6 +14,7 @@ const CheckboxPage = () => {
         <FloatingActionButtons />,
         <FloatingActionButtonSize />,
         <FloatingActionButtonExtendedSize />,
+        <FloatingActionButtonZoom />,
     ];
 
     return (
