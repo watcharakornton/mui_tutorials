312a02d6520facc3a3a2c350d28ea1374054202e
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-17 23:27:27 +0700

    Add Basic Text Fields into TextField Component

--- src/inputs/TextFieldComponent.jsx
+++ src/inputs/TextFieldComponent.jsx
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import {
     Typography,
+    Box,
+    TextField,
 } from '@mui/material';
 
 export const BasicTextFields = () => {
     return (
-       <>
-        <Typography variant='body1' sx={{ mb: 3 }}>Basic Text Fields</Typography>
-       </> 
+        <>
+            <Typography variant='body1' sx={{ mb: 3 }}>Basic Text Fields</Typography>
+            <Box
+                component="form"
+                sx={{ '& > :not(style)': { m: 1, width: '25ch' }}}
+                noValidate
+                autoComplete='off'
+            >
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                <TextField id="filled-basic" label="Filled" variant="filled" />
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+            </Box>
+        </>
     )
 }
