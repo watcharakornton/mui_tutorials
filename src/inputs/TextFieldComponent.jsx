457ac8dff9908d07d45710e4f83d1ea4dc301305
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-18 18:22:09 +0700

    Add Multiline Text Fields into TextFields Component

--- src/inputs/TextFieldComponent.jsx
+++ src/inputs/TextFieldComponent.jsx
@@ -7,6 +7,82 @@ export const MultilineTextFields = () => {
       <Typography variant="h5" sx={{ mb: 3 }} fontWeight="fontWeightMedium">
         Multiline Text Fields Component
       </Typography>
+      <Box
+        component="form"
+        sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Multiline"
+            multiline
+            maxRows={4}
+          />
+          <TextField
+            id="outlined-textarea"
+            label="Multiline Placeholder"
+            placeholder="Placeholder"
+            multiline
+          />
+          <TextField
+            id="outlined-multiline-static"
+            label="Multiline"
+            multiline
+            rows={4}
+            defaultValue="Default Value"
+          />
+        </div>
+        <div>
+          <TextField
+            id="filled-multiline-flexible"
+            label="Multiline"
+            multiline
+            maxRows={4}
+            variant="filled"
+          />
+          <TextField
+            id="filled-textarea"
+            label="Multiline Placeholder"
+            placeholder="Placeholder"
+            multiline
+            variant="filled"
+          />
+          <TextField
+            id="filled-multiline-static"
+            label="Multiline"
+            multiline
+            rows={4}
+            defaultValue="Default Value"
+            variant="filled"
+          />
+        </div>
+        <div>
+          <TextField
+            id="standard-multiline-flexible"
+            label="Multiline"
+            multiline
+            maxRows={4}
+            variant="standard"
+          />
+          <TextField
+            id="standard-textarea"
+            label="Multiline Placeholder"
+            placeholder="Placeholder"
+            multiline
+            variant="standard"
+          />
+          <TextField
+            id="standard-multiline-static"
+            label="Multiline"
+            multiline
+            rows={4}
+            defaultValue="Default Value"
+            variant="standard"
+          />
+        </div>
+      </Box>
     </>
   );
 };
