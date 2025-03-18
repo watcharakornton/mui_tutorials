c1ed7fc90b59e6652ecaa88f62ff6ce099586e5d
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-18 18:15:47 +0700

    Add Validation TextFields into Text Fields Component

--- src/inputs/TextFieldComponent.jsx
+++ src/inputs/TextFieldComponent.jsx
@@ -1,6 +1,82 @@
 import * as React from "react";
 import { Typography, Box, TextField } from "@mui/material";
 
+export const MultilineTextFields = () => {
+  return (
+    <>
+      <Typography variant="h5" sx={{ mb: 3 }} fontWeight="fontWeightMedium">
+        Multiline Text Fields Component
+      </Typography>
+    </>
+  );
+};
+
+export const ValidationTextFields = () => {
+  return (
+    <>
+      <Typography variant="h5" sx={{ mb: 3 }} fontWeight="fontWeightMedium">
+        Validation Text Fields
+      </Typography>
+      <Box
+        component="form"
+        sx={{ "& .MuiTextField-root": { m: 1, width: "25ch" } }}
+        noValidate
+        autoComplete="off"
+      >
+        <div>
+          <TextField
+            error
+            id="outlined-error"
+            label="Error"
+            defaultValue="Hello World"
+          />
+          <TextField
+            error
+            id="outlined-error-helper-text"
+            label="Error"
+            defaultValue="Hello World"
+            helperText="Incorrect entry."
+          />
+        </div>
+        <div>
+          <TextField
+            error
+            id="filled-error"
+            label="Error"
+            defaultValue="Hello World"
+            variant="filled"
+          />
+          <TextField
+            error
+            id="filled-error-helper-text"
+            label="Error"
+            defaultValue="Hello World"
+            helperText="Incorrect entry"
+            variant="filled"
+          />
+        </div>
+        <div>
+          <TextField
+            error
+            id="standard-error"
+            label="Error"
+            defaultValue="Hello World"
+            variant="standard"
+          />
+          <TextField
+            error
+            id="standard-error-helper-text"
+            label="Error"
+            defaultValue="Hello World"
+            helperText="Incorrect entry."
+            variant="standard"
+          />
+        </div>
+      </Box>
+    </>
+  );
+};
+
 export const FormPropsTextFields = () => {
   return (
     <>
--- src/pages/TextFieldPage.jsx
+++ src/pages/TextFieldPage.jsx
@@ -1,53 +1,52 @@
-import * as React from 'react'
-import {
-    Container,
-    Grid,
-    Typography,
-    Button,
-    Divider,
-} from '@mui/material';
-import {
-    ChevronLeft as ChevronLeftIcon
-} from '@mui/icons-material';
+import * as React from "react";
+import { Container, Grid, Typography, Button, Divider } from "@mui/material";
+import { ChevronLeft as ChevronLeftIcon } from "@mui/icons-material";
 
 import {
-    BasicTextFields,
-    FormPropsTextFields,
-} from '../inputs/TextFieldComponent';
+  BasicTextFields,
+  FormPropsTextFields,
+  ValidationTextFields,
+  MultilineTextFields,
+} from "../inputs/TextFieldComponent";
 
 const TextFieldPage = () => {
-    const sections = [
-        <BasicTextFields />,
-        <FormPropsTextFields/>,
-    ];
+  const sections = [
+    <BasicTextFields />,
+    <FormPropsTextFields />,
+    <ValidationTextFields />,
+    <MultilineTextFields />,
+  ];
 
-    return (
-        <Container maxWidth="sm">
-            <Grid
-                container
-                alignItems="center"
-                justifyContent="center"
-                sx={{ minHeight: "100vh" }}
-            >
-                <Grid item xs={12}>
-                    <Typography variant='h3' align='center' sx={{ mt: 5 }}>
-                        Text Fields Page
-                    </Typography>
-                </Grid>
-                {sections.map((Component, index) => (
-                    <React.Fragment key={index}>
-                        <Grid item xs={12} sx={{ mt: 5 }}>
-                            {Component}
-                            <Divider sx={{ mt: 3, width: "100%" }} />
-                        </Grid>
-                    </React.Fragment>
-                ))}
-                <Grid item xs={12} sx={{ my: 5 }}>
-                    <Button size='large' startIcon={<ChevronLeftIcon />} href="/">Back</Button>
-                </Grid>
+  return (
+    <Container maxWidth="sm">
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h3" align="center" sx={{ mt: 5 }}>
+            Text Fields Page
+          </Typography>
+        </Grid>
+        {sections.map((Component, index) => (
+          <React.Fragment key={index}>
+            <Grid item xs={12} sx={{ mt: 5 }}>
+              {Component}
+              <Divider sx={{ mt: 3, width: "100%" }} />
             </Grid>
-        </Container>
-    )
-}
+          </React.Fragment>
+        ))}
+        <Grid item xs={12} sx={{ my: 5 }}>
+          <Button size="large" startIcon={<ChevronLeftIcon />} href="/">
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default TextFieldPage
+export default TextFieldPage;
+
