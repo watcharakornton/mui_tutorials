32e6f38544c38d0d31668ef433541013bb89162e
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-18 15:08:36 +0700

    Add Customized Slider into Slider Component

--- src/pages/SliderPage.jsx
+++ src/pages/SliderPage.jsx
@@ -1,69 +1,68 @@
-import * as React from 'react'
-import {
-    Container,
-    Grid,
-    Typography,
-    Button,
-    Divider,
-} from '@mui/material';
-import {
-    ChevronLeft as ChevronLeftIcon
-} from '@mui/icons-material';
+import * as React from "react";
+import { Container, Grid, Typography, Button, Divider } from "@mui/material";
+import { ChevronLeft as ChevronLeftIcon } from "@mui/icons-material";
 
 import {
-    ContinuousSlider,
-    SliderSizes,
-    DiscreteSlider,
-    DiscreteSliderSteps,
-    DiscreteSliderMarks,
-    DiscreteSliderValues,
-    DiscreteSliderLabel,
-    RangeSlider,
-    MinimumDistanceSlider,
-    InputSlider,
-} from '../inputs/SliderComponent';
+  ContinuousSlider,
+  SliderSizes,
+  DiscreteSlider,
+  DiscreteSliderSteps,
+  DiscreteSliderMarks,
+  DiscreteSliderValues,
+  DiscreteSliderLabel,
+  RangeSlider,
+  MinimumDistanceSlider,
+  InputSlider,
+  ColorSlider,
+  CustomizedSlider,
+} from "../inputs/SliderComponent";
 
 const SliderPage = () => {
-    const sections = [
-        <ContinuousSlider />,
-        <SliderSizes />,
-        <DiscreteSlider />,
-        <DiscreteSliderSteps />,
-        <DiscreteSliderMarks />,
-        <DiscreteSliderValues />,
-        <DiscreteSliderLabel />,
-        <RangeSlider />,
-        <MinimumDistanceSlider />,
-        <InputSlider />,
-    ];
+  const sections = [
+    <ContinuousSlider />,
+    <SliderSizes />,
+    <DiscreteSlider />,
+    <DiscreteSliderSteps />,
+    <DiscreteSliderMarks />,
+    <DiscreteSliderValues />,
+    <DiscreteSliderLabel />,
+    <RangeSlider />,
+    <MinimumDistanceSlider />,
+    <InputSlider />,
+    <ColorSlider />,
+    <CustomizedSlider />,
+  ];
 
-    return (
-        <Container maxWidth="sm">
-            <Grid
-                container
-                alignItems="center"
-                justifyContent="center"
-                sx={{ minHeight: "100vh" }}
-            >
-                <Grid item xs={12}>
-                    <Typography variant='h3' align='center' sx={{ mt: 5 }}>
-                        Slider Page
-                    </Typography>
-                </Grid>
-                {sections.map((Component, index) => (
-                    <React.Fragment key={index}>
-                        <Grid item xs={12} sx={{ mt: 5 }}>
-                            {Component}
-                            <Divider sx={{ mt: 3, width: "100%" }} />
-                        </Grid>
-                    </React.Fragment>
-                ))}
-                <Grid item xs={12} sx={{ my: 5 }}>
-                    <Button size='large' startIcon={<ChevronLeftIcon />} href="/">Back</Button>
-                </Grid>
+  return (
+    <Container maxWidth="sm">
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h3" align="center" sx={{ mt: 5 }}>
+            Slider Page
+          </Typography>
+        </Grid>
+        {sections.map((Component, index) => (
+          <React.Fragment key={index}>
+            <Grid item xs={12} sx={{ mt: 5 }}>
+              {Component}
+              <Divider sx={{ mt: 3, width: "100%" }} />
             </Grid>
-        </Container>
-    )
-}
+          </React.Fragment>
+        ))}
+        <Grid item xs={12} sx={{ my: 5 }}>
+          <Button size="large" startIcon={<ChevronLeftIcon />} href="/">
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default SliderPage
+export default SliderPage;
+
