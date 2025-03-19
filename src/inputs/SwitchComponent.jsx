c1d4c8e85caeb9f3cb6360e331574f7745f0f2ec
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-19 11:08:48 +0700

    Add SwitchLabels and Switches Size Fn into Switch Component

--- src/inputs/SwitchComponent.jsx
+++ src/inputs/SwitchComponent.jsx
@@ -1,12 +1,52 @@
-import * as React from 'react';
-import {
-    Typography,
-} from '@mui/material';
+import * as React from "react";
+import { Typography, Switch, FormGroup, FormControlLabel } from "@mui/material";
+
+const label = [
+  { inputProps: { "aria-label": "Switch demo" } },
+  { inputProps: { "aria-label": "Size switch demo" } },
+];
+
+export const SwitchesSizes = () => {
+  return (
+    <>
+      <Typography variant="h5" sx={{ mb: 3 }}>
+        Switches Sizes
+      </Typography>
+      <div>
+        <Switch {...label[1]} defaultChecked size="small" />
+        <Switch {...label[1]} defaultChecked />
+      </div>
+    </>
+  );
+};
+
+export const SwitchLabels = () => {
+  return (
+    <>
+      <Typography variant="h5" sx={{ mb: 3 }}>
+        Switch Labels
+      </Typography>
+      <FormGroup>
+        <FormControlLabel control={<Switch defaultChecked />} label="Label" />
+        <FormControlLabel required control={<Switch />} label="Required" />
+        <FormControlLabel disabled control={<Switch />} label="Disabled" />
+      </FormGroup>
+    </>
+  );
+};
 
 export const BasicSwitches = () => {
-    return (
-        <>
-            <Typography variant='body1' sx={{ mb: 3 }}>Basic Switches</Typography>
-        </>
-    )
-}
+  return (
+    <>
+      <Typography variant="h5" sx={{ mb: 3 }}>
+        Basic Switches
+      </Typography>
+      <div>
+        <Switch {...label[0]} defaultChecked />
+        <Switch {...label[0]} />
+        <Switch {...label[0]} disabled defaultChecked />
+        <Switch {...label[0]} disabled />
+      </div>
+    </>
+  );
+};
--- src/pages/SwitchPage.jsx
+++ src/pages/SwitchPage.jsx
@@ -1,51 +1,46 @@
-import * as React from 'react'
-import {
-    Container,
-    Grid,
-    Typography,
-    Button,
-    Divider,
-} from '@mui/material';
-import {
-    ChevronLeft as ChevronLeftIcon
-} from '@mui/icons-material';
+import * as React from "react";
+import { Container, Grid, Typography, Button, Divider } from "@mui/material";
+import { ChevronLeft as ChevronLeftIcon } from "@mui/icons-material";
 
 import {
-    BasicSwitches,
-} from '../inputs/SwitchComponent';
+  BasicSwitches,
+  SwitchLabels,
+  SwitchesSizes,
+} from "../inputs/SwitchComponent";
 
 const SwitchPage = () => {
-    const sections = [
-        <BasicSwitches />,
-    ];
+  const sections = [<BasicSwitches />, <SwitchLabels />, <SwitchesSizes />];
 
-    return (
-        <Container maxWidth="sm">
-            <Grid
-                container
-                alignItems="center"
-                justifyContent="center"
-                sx={{ minHeight: "100vh" }}
-            >
-                <Grid item xs={12}>
-                    <Typography variant='h3' align='center' sx={{ mt: 5 }}>
-                        Switch Page
-                    </Typography>
-                </Grid>
-                {sections.map((Component, index) => (
-                    <React.Fragment key={index}>
-                        <Grid item xs={12} sx={{ mt: 5 }}>
-                            {Component}
-                            <Divider sx={{ mt: 3, width: "100%" }} />
-                        </Grid>
-                    </React.Fragment>
-                ))}
-                <Grid item xs={12} sx={{ my: 5 }}>
-                    <Button size='large' startIcon={<ChevronLeftIcon />} href="/">Back</Button>
-                </Grid>
+  return (
+    <Container maxWidth="sm">
+      <Grid
+        container
+        alignItems="center"
+        justifyContent="center"
+        sx={{ minHeight: "100vh" }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h3" align="center" sx={{ mt: 5 }}>
+            Switch Page
+          </Typography>
+        </Grid>
+        {sections.map((Component, index) => (
+          <React.Fragment key={index}>
+            <Grid item xs={12} sx={{ mt: 5 }}>
+              {Component}
+              <Divider sx={{ mt: 3, width: "100%" }} />
             </Grid>
-        </Container>
-    )
-}
+          </React.Fragment>
+        ))}
+        <Grid item xs={12} sx={{ my: 5 }}>
+          <Button size="large" startIcon={<ChevronLeftIcon />} href="/">
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
 
-export default SwitchPage
+export default SwitchPage;
+
