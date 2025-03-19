5ce3d63749e68faafab064b71f11a41847b239d9
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-19 17:06:13 +0700

    Add ToggleButtons Module into ToggleButton Component

--- src/inputs/ToggleButtonComponent.jsx
+++ src/inputs/ToggleButtonComponent.jsx
@@ -1,12 +1,62 @@
 import * as React from "react";
-import { Typography } from "@mui/material";
+import {
+  Typography,
+  Box,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import {
+  FormatAlignLeft as FormatAlignLeftIcon,
+  FormatAlignCenter as FormatAlignCenterIcon,
+  FormatAlignRight as FormatAlignRightIcon,
+  FormatAlignJustify as FormatAlignJustifyIcon,
+} from "@mui/icons-material";
 
 export const ToggleButtons = () => {
+  const [alignment, setAlignment] = React.useState("left");
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
+
   return (
     <>
       <Typography variant="h5" sx={{ mb: 3 }}>
         Toggle Buttons
       </Typography>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100px",
+          border: "1px solid gray",
+          borderRadius: "10px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box>
+          <ToggleButtonGroup
+            value={alignment}
+            exclusive
+            onChange={handleAlignment}
+            aria-label="text alignment"
+          >
+            <ToggleButton value="left" aria-label="left aligned">
+              <FormatAlignLeftIcon />
+            </ToggleButton>
+            <ToggleButton value="center" aria-label="centered">
+              <FormatAlignCenterIcon />
+            </ToggleButton>
+            <ToggleButton value="right" aria-label="right aligned">
+              <FormatAlignRightIcon />
+            </ToggleButton>
+            <ToggleButton value="justify" aria-label="justified" disabled>
+              <FormatAlignJustifyIcon />
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Box>
+      </Box>
     </>
   );
 };
