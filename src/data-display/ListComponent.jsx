28d6652e6e0cec9536464a6a3b281f9d1ae11d51
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-25 17:08:39 +0700

    Add CheckboxListSecondary module into List component

--- src/data-display/ListComponent.jsx
+++ src/data-display/ListComponent.jsx
@@ -37,9 +37,52 @@ import {
 } from '@mui/icons-material';
 
 export const CheckboxListSecondary = () => {
+    const [checked, setChecked] = React.useState([1]);
+
+    const handleToggle = (value) => () => {
+        const currentIndex = checked.indexOf(value);
+        const newChecked = [...checked];
+
+        if (currentIndex === -1) {
+            newChecked.push(value);
+        } else {
+            newChecked.splice(currentIndex, 1);
+        }
+
+        setChecked(newChecked);
+    };
+
     return (
         <MyContainer title="Checkbox List Secondary">
-
+            <List dense sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+                {[0, 1, 2, 3].map((value) => {
+                    const labelId = `checkbox-list-secondary-label-${value}`;
+                    return (
+                        <ListItem
+                            key={value}
+                            secondaryAction={
+                                <Checkbox
+                                    edge="end"
+                                    onChange={handleToggle(value)}
+                                    checked={checked.includes(value)}
+                                    inputProps={{ 'aria-labelledby': labelId }}
+                                />
+                            }
+                            disablePadding
+                        >
+                            <ListItemButton>
+                                <ListItemAvatar>
+                                    <Avatar 
+                                        alt={`Avatar n°${value + 1}`}
+                                        src={`src/assets/images/avatar/${value + 1}.jpg`}
+                                    />
+                                </ListItemAvatar>
+                                <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                            </ListItemButton>
+                        </ListItem>
+                    )
+                })}
+            </List>
         </MyContainer>
     )
 }
