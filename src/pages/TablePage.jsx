6bf5f7408ae317e40bf059b0e4cce8d5dd16374f
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-27 17:13:11 +0700

    Add AccessibleTable module into Table component

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -14,6 +14,7 @@ import {
     CollapsibleTable,
     SpanningTable,
     ReactVirtualizedTable,
+    AccessibleTable,
 } from "../data-display/TableComponent";
 
 const TablePage = () => {
@@ -29,6 +30,7 @@ const TablePage = () => {
         <CollapsibleTable />,
         <SpanningTable />,
         <ReactVirtualizedTable />,
+        <AccessibleTable />,
     ];
 
     return (
