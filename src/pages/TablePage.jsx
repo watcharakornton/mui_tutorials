62b8216ff2cadd5f0769c0b8e2730f03979bba01
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-27 16:19:54 +0700

    Add ColumnGroupingTable module into Table component

--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -10,6 +10,7 @@ import {
     CustomizedTables,
     CustomPaginationActionsTable,
     StickyHeadTable,
+    ColumnGroupingTable,
 } from "../data-display/TableComponent";
 
 const TablePage = () => {
@@ -21,6 +22,7 @@ const TablePage = () => {
         <CustomizedTables />,
         <CustomPaginationActionsTable />,
         <StickyHeadTable />,
+        <ColumnGroupingTable />,
     ];
 
     return (
