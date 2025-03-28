99e6d5ecdf00d264f9c09bc5c0ad603fef4d2a66
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-28 20:31:00 +0700

    SimpleBackdrop module into Backdrop component

--- src/feedback/BackdropComponent.jsx
+++ src/feedback/BackdropComponent.jsx
@@ -1,10 +1,32 @@
 import * as React from 'react';
 import MyContainer from '../component/MyContainer';
+import {
+    Backdrop,
+    CircularProgress,
+    Button,
+} from '@mui/material';
 
 export const SimpleBackdrop = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
     return (
         <MyContainer title="Simple Backdrop">
-
+            <div>
+                <Button onClick={handleOpen}>Show backdrop</Button>
+                <Backdrop
+                    sx={(theme) => ({ color: '#fff', zIndex: theme.zIndex.drawer + 1 })}
+                    open={open}
+                    onClick={handleClose}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+            </div>
         </MyContainer>
     )
 }
