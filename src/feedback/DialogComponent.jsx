b9a826168dc28bdedb418c86026d201c142fa389
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-28 20:37:49 +0700

    Add AlertDialog module into Dialog component

--- src/feedback/DialogComponent.jsx
+++ src/feedback/DialogComponent.jsx
@@ -1,10 +1,54 @@
 import * as React from 'react';
 import MyContainer from '../component/MyContainer';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from '@mui/material';
 
 export const AlertDialog = () => {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <MyContainer title="Alert Dialog">
-
+            <React.Fragment>
+                <Button variant='outlined' onClick={handleClickOpen}>
+                    Open alert dialog
+                </Button>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby='alert-dialopg-title'
+                    aria-describedby='alert-dialog-description'
+                >
+                    <DialogTitle id="alert-dialog-title">
+                        {"Use Google's location service?"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            Let Google help apps determine location. This means sending anonymous
+                            location data to Google, even when no apps are running.
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Disagree</Button>
+                        <Button onClick={handleClose} autoFocus>
+                            Agree
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </React.Fragment>
         </MyContainer>
     )
 }
