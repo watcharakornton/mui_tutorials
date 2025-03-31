d88c8c41d9c1c36bd60fc83bf56871ecdfda1f64
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-31 23:13:56 +0700

    Add SimpleSnackbar module into Snackbar component

--- src/feedback/SnackbarComponent.jsx
+++ src/feedback/SnackbarComponent.jsx
@@ -1,8 +1,57 @@
 import * as React from 'react'
 import MyContainer from '../component/MyContainer'
+import {
+    Button,
+    Snackbar,
+    IconButton,
+} from '@mui/material';
+import {
+    Close as CloseIcon
+} from '@mui/icons-material';
 
 export const SimpleSnackbar = () => {
-  return (
-    <MyContainer title="Simple Snackbar"></MyContainer>
-  )
+    const [open, setOpen] = React.useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+         
+        setOpen(false);
+    };
+
+    const action = (
+        <React.Fragment>
+            <Button color="secondary" size="small" onClick={handleClose}>
+                UNDO
+            </Button>
+            <IconButton 
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+                <CloseIcon fontSize="small" />
+            </IconButton>
+        </React.Fragment>
+    );
+
+    return (
+        <MyContainer title="Simple Snackbar">
+            <div>
+                <Button onClick={handleClick}>Open Snackbar</Button>
+                <Snackbar 
+                    open={open}
+                    autoHideDuration={6000}
+                    onClose={handleClose}
+                    message="Note archived"
+                    action={action}
+                />
+            </div>
+        </MyContainer>
+    );
 }
