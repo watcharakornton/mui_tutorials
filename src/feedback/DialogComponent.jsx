b3e7da5a1b2e22cce1950d25a16ba643ee8872ba
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-03-31 17:45:29 +0700

    Add MaxWidthDialog module into Dialog component

--- src/feedback/DialogComponent.jsx
+++ src/feedback/DialogComponent.jsx
@@ -2,19 +2,26 @@ import * as React from 'react';
 import MyContainer from '../component/MyContainer';
 import { styled, useTheme } from '@mui/material/styles';
 import {
+    Box,
     Button,
     TextField,
     Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    FormControl,
+    FormControlLabel,
+    InputLabel,
+    MenuItem,
+    Select,
+    Switch,
     ListItemText,
     ListItemButton,
     List,
     Divider,
     AppBar,
     Toolbar,
-    DialogActions,
-    DialogContent,
-    DialogContentText,
-    DialogTitle,
     Slide,
     IconButton,
     Typography,
@@ -22,6 +29,94 @@ import {
 import {
     Close as CloseIcon
 } from '@mui/icons-material';
+
+export const MaxWidthDialog = () => {
+    const [open, setOpen] = React.useState(false);
+    const [fullWidth, setFullWidth] = React.useState(true);
+    const [maxWidth, setMaxWidth] = React.useState('sm');
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleMaxWidthChange = (event) => {
+        setMaxWidth(
+            event.target.value,
+        );
+    };
+
+    const handleFullWidthChange = (event) => {
+        setFullWidth(event.target.checked);
+    };
+
+    return (
+        <MyContainer title="Max Width Dialog">
+            <React.Fragment>
+                <Button variant='outlined' onClick={handleClickOpen}>
+                    Open max-width dialog
+                </Button>
+                <Dialog
+                    fullWidth={fullWidth}
+                    maxWidth={maxWidth}
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <DialogTitle>Optional sizes</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            You can set my maximum width and whether to adapt or not.
+                        </DialogContentText>
+                        <Box
+                            noValidate
+                            component="form"
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                m: 'auto',
+                                width: 'fit-content',
+                            }}
+                        >
+                            <FormControl sx={{ mt: 2, minWidth: 120 }}>
+                                <InputLabel htmlFor="max-width">maxWidth</InputLabel>
+                                <Select
+                                    autoFocus
+                                    value={maxWidth}
+                                    onChange={handleMaxWidthChange}
+                                    label="maxWidth"
+                                    inputProps={{
+                                        name: 'max-width',
+                                        id: 'max-width',
+                                    }}
+                                >
+                                    <MenuItem value={false}>false</MenuItem>
+                                    <MenuItem value="xs">xs</MenuItem>
+                                    <MenuItem value="sm">sm</MenuItem>
+                                    <MenuItem value="md">md</MenuItem>
+                                    <MenuItem value="lg">lg</MenuItem>
+                                    <MenuItem value="xl">xl</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControlLabel
+                                sx={{ mt: 1 }}
+                                control={
+                                    <Switch checked={fullWidth} onChange={handleFullWidthChange} />
+                                }
+                                label="Full width"
+                            />
+                        </Box>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Close</Button>
+                    </DialogActions>
+                </Dialog>
+            </React.Fragment>
+        </MyContainer>
+    )
+}
 
 export const FullScreenDialog = () => {
     const [open, setOpen] = React.useState(false);
--- src/pages/DialogPage.jsx
+++ src/pages/DialogPage.jsx
@@ -8,6 +8,7 @@ import {
     FormDialog,
     CustomizedDialogs,
     FullScreenDialog,
+    MaxWidthDialog,
 } from "../feedback/DialogComponent";
 
 const DialogPage = () => {
@@ -17,6 +18,7 @@ const DialogPage = () => {
         <FormDialog />,
         <CustomizedDialogs />,
         <FullScreenDialog />,
+        <MaxWidthDialog />,
     ];
 
     return (
