ac5b6e0a75b86e400512a40962dd17ef56681b5e
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-04-05 21:44:05 +0700

    Add BasicBreadcrumbs component into Breadcrumbs page

--- src/navigation/BreadcrumbsComponent.jsx
+++ src/navigation/BreadcrumbsComponent.jsx
@@ -1,8 +1,34 @@
 import * as React from 'react';
 import MyContainer from '../component/MyContainer';
+import {
+    Typography,
+    Breadcrumbs,
+    Link,
+} from '@mui/material/';
 
 export const BasicBreadcrumbs = () => {
+    function handleClick(event) {
+        event.preventDefault();
+        console.info('You clicked a breadcrumb.');
+    }
+
     return (
-        <MyContainer title="Basic Breadcrumbs"></MyContainer>
+        <MyContainer title="Basic Breadcrumbs">
+            <div rolw="presentation" onClick={handleClick}>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <Link underline='hover' color="inherit" href="/">
+                        MUI
+                    </Link>
+                    <Link
+                        underline='hover'
+                        color="inherit"
+                        href="/material-ui/getting-started/installation/"
+                    >
+                        Core
+                    </Link>
+                    <Typography sx={{ color: 'text.primary' }}>Breadcrumbs</Typography>
+                </Breadcrumbs>
+            </div>
+        </MyContainer>
     )
 }
