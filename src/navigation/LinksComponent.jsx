bcc91da5e783f962199e68835f472daae4caec73
Watcharakorn Jantarasangaram (SI) <watcharakorn.j@Watcharakorns-MacBook-Air.local>
2025-04-09 17:43:32 +0700

    Add Links module into Links component

--- src/navigation/LinksComponent.jsx
+++ src/navigation/LinksComponent.jsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 import MyContainer from '../component/MyContainer';
+import {
+    Box,
+    Link,
+} from '@mui/material';
+
+const preventDefault = (event) => event.preventDefault();
 
 export const Links = () => {
     return (
-        <MyContainer title="Links"></MyContainer>
+        <MyContainer title="Links">
+            <Box
+                sx={{
+                    typography: 'body1',
+                    '& > :not(style) ~ :not(style)': {
+                        ml: 2,
+                    },
+                }}
+                onClick={preventDefault}
+            >
+                <Link href="#">Link</Link>
+                <Link href="#" color="inherit">
+                    {'color="inherit"'}
+                </Link>
+                <Link href="#" variant='body2'>
+                    {'variant="body2"'}
+                </Link>
+            </Box>
+        </MyContainer>
     )
 }
